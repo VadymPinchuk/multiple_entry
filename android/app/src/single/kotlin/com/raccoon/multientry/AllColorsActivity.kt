ea5de9f0996aa10eac098a7175f6e3d0d9d097df
Vadym Pinchuk <vad.pinchuk@gmail.com>
2023-03-31 22:49:39 +0100

    Cleaned activities

--- android/app/src/single/kotlin/com/raccoon/multientry/AllColorsActivity.kt
+++ android/app/src/single/kotlin/com/raccoon/multientry/AllColorsActivity.kt
@@ -1,13 +1,5 @@
 package com.raccoon.multientry
 
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class AllColorsActivity : FlutterActivity() {
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) =
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-
-}
+class AllColorsActivity : FlutterActivity() {}
